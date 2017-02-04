2156e27acf5e807402f038deb8cefb88435dd73c
ledomone <ledom.git@gmail.com>
2017-02-04 21:39:41 +0100

    Password hashing with bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -31,3 +31,21 @@ const UserSchema = new Schema({
 {
   timestamps: true
 });
+
+// Pre-save of user to database, hash password if password is modified or new
+UserSchema.pre('save', function(next) {
+  const user = this,
+        SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
