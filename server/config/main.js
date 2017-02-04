182645104a3368c0a3a6b09b97a2694091edb47d
ledomone <ledom.git@gmail.com>
2017-02-04 22:01:35 +0100

    Add Passport JWT Strategy

--- server/config/main.js
+++ server/config/main.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // Secret key for JWT signing and encryption
+  'secret': 'super secret passphrase',
   // Database connection information
   'database': 'mongodb://localhost:27017',
   // Setting port for server
--- server/config/passport.js
+++ server/config/passport.js
@@ -23,3 +23,28 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
     });
   });
 });
+
+// Setting JWT strategy options
+const jwtOptions = {
+  // Telling Passport to check authorization headers for JWT
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  // Telling Passport where to find the secret
+  secretOrKey: config.secret
+};
+
+// Setting up JWT login strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload._id, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Allow passport to use our strategies
+passport.use(jwtLogin);
+passport.use(localLogin);
