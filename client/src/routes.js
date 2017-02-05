2b0e797c22540586a5f5c1c73b1522c54d3bbef3
ledomone <ledom.git@gmail.com>
2017-02-05 17:46:35 +0100

    Add App component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-// import App from './components/app';
+import App from './components/app';
 // import NotFoundPage from './components/pages/not-found-page';
 
 // import HomePage from './components/pages/home-page';
