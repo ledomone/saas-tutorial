77ff5e4c432305c6464922ae28e6194dc96409af
ledomone <ledom.git@gmail.com>
2017-02-05 18:02:46 +0100

    Add HomePage component

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,7 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import NotFoundPage from './components/pages/not-found-page';
 
-// import HomePage from './components/pages/home-page';
+import HomePage from './components/pages/home-page';
 // import Register from './components/auth/register';
 // import Login from './components/auth/login';
 import Dashboard from './components/dashboard';
