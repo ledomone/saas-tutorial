97f2d83f09210f9c8772a4488077813b8f6639a1
ledomone <ledom.git@gmail.com>
2017-02-05 17:08:27 +0100

    Defining routes - use in router

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
-// import routes from './routes';
+import routes from './routes';
 // import reducers from './reducers/index';
 // import { AUTH_USER } from './actions/types';
 
