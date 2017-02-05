3a991a6e73e6f5b8b6c0ab5499e58a1471ef5700
ledomone <ledom.git@gmail.com>
2017-02-05 14:02:33 +0100

    Add chat routes in router

--- server/router.js
+++ server/router.js
@@ -1,4 +1,5 @@
 const AuthenticationController = require('./controllers/authentication'),
+      ChatController = require('./controllers/chat'),
       express = require('express'),
       passportService = require('./config/passport'),
       passport = require('passport');
@@ -17,6 +18,7 @@ module.exports = function(app) {
   // Initializing route groups
   const apiRoutes = express.Router();
   const authRoutes = express.Router();
+  const chatRoutes = express.Router();
 
   //=================================
   // Auth Routes
@@ -30,6 +32,25 @@ module.exports = function(app) {
 
   // Login route
   authRoutes.post('/login', requireLogin, AuthenticationController.login);
+
+  //=================================
+  // Chat Routes
+  //=================================
+
+  // Set chat routes as a subgroup/middleware to apiRoutes
+  apiRoutes.use('/chat', chatRoutes);
+
+  // View messages to and from authenticated user
+  chatRoutes.get('/', requireAuth, ChatController.getConversations);
+
+  // Retrieve single conversation
+  chatRoutes.get('/:conversationId', requireAuth, ChatController.getConversation);
+
+  // Send reply in conversation
+  chatRoutes.post('/:conversationId', requireAuth, ChatController.sendReply);
+
+  // Start new conversation
+  chatRoutes.post('/new/:recipient', requireAuth, ChatController.newConversation);
 
   // Set url for API group routes
   app.use('/api', apiRoutes);
