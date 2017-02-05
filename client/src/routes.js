fa66c822fb7f2d9da0e80d18fd7a0c2ada001bea
ledomone <ledom.git@gmail.com>
2017-02-05 23:42:51 +0100

    Creating a Higher Order Component (RequireAuth)

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,14 +8,14 @@ import HomePage from './components/pages/home-page';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
 import Dashboard from './components/dashboard';
-// import RequiredAuth from './components/auth/require-auth';
+import RequireAuth from './components/auth/require-auth';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="register" component={Register} />
     <Route path="login" component={Login} />
-    <Route path="dashboard" component={Dashboard} />
+    <Route path="dashboard" component={RequireAuth(Dashboard)} />
 
     <Route path="*" component={NotFoundPage} />
   </Route>
